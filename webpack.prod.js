48a438513a915b784a87c367ed92030904097402
christianquantumpix <89075034+christianquantumpix@users.noreply.github.com>
2023-01-05 21:04:10 +0100

    Added functionality for production build.

---
+++ webpack.prod.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+    entry: path.resolve(__dirname, "src/index.ts"),
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                include: [path.resolve(__dirname, "src")]
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: [".js", ".ts"]
+    },
+
+    output: {
+        publicPath: "dist",
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+
+    devtool: "source-map",
+    mode: "production",
+}
